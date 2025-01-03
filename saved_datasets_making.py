834397c918e37bd140f69ea4d2b1d5706a0dcc3e
jianyao05 <kiatyaw05@gmail.com>
2025-01-03 16:28:36 +0800

    20250103 afternoon 1628PM MADE YUHANG DO AS WELL

--- saved_datasets_making.py
+++ saved_datasets_making.py
@@ -18,14 +18,13 @@ frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # Parameters
 #label = "ARMPIT_LEFT"  # Label for the dataset (change for specific movement)
-label = "ARMPIT_RIGHT"  # Label for the dataset (change for specific movement)
 #label = "CIRCLE_LEFT"  # Label for the dataset (change for specific movement)
 #label = "CIRCLE_RIGHT"  # Label for the dataset (change for specific movement)
 #label = "CROSS_BODY_LEFT"  # Label for the dataset (change for specific movement)
 #label = "CROSS_BODY_RIGHT"  # Label for the dataset (change for specific movement)
 #label = "PENDULUM_LEFT"  # Label for the dataset (change for specific movement)
 #label = "PENDULUM_RIGHT"  # Label for the dataset (change for specific movement)
-#label = "SHOULDER_FLEXION_LEFT"  # Label for the dataset (change for specific movement)
+label = "SHOULDER_FLEXION_LEFT"  # Label for the dataset (change for specific movement)
 #label = "SHOULDER_FLEXION_RIGHT"  # Label for the dataset (change for specific movement)
 
 n_repetitions_to_add = 50  # Number of repetitions to add
